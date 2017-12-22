18ee525e12f628f7cfddb1dbef8b431e4a6eb7c8
Ryan Naddy <rnaddy@corp.acesse.com>
2017-12-22 14:19:27 -0600

    updated row setting of values

--- src/Row.ts
+++ src/Row.ts
@@ -4,12 +4,15 @@ import { ModelItems } from "./ModelBase";
 //   [key: string]: T
 // }// & {[P in T]}
 
+export interface Row<T extends ModelItems> {
+  [key: string]: T
+}
+
 export class Row<T extends ModelItems> {
 
   private _row: T = <T>{}
   private _dirty: boolean = false
   private _newRow: boolean = true
-  private pointer: number = 0
 
   public get row() { return this._row }
   public get isDirty() { return this._dirty }
@@ -32,26 +35,14 @@ export class Row<T extends ModelItems> {
         return (<any>target)[prop] || target._row[prop] || ''
       },
       set: function (target, prop, value) {
-        target._row[prop] = value
+        if (['_row', '_dirty', '_newRow'].indexOf(prop.toString()) == -1) {
+          target._row[prop] = value
+        } else {
+          (<any>target)[prop] = value
+        }
         return true
-        // if (target._row[prop]) {
-        //   target._row[prop] = value
-        //   return true
-        // }
-        // else {
-        //   (<any>target)[prop] = value
-        // }
-        // return false
       }
     })
   }
 
-  // public next(): IteratorResult<Row<T>> {
-  //   if (this.pointer < this.rows.length) {
-  //     return { done: false, value: this.rows[this.pointer++] }
-  //   } else {
-  //     this.pointer = 0
-  //     return { done: true, value: undefined } as any as IteratorResult<Row<T>>
-  //   }
-  // }
 }
